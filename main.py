009f2a24d027d5c0f5110eb03065bb0a26ea4440
Jay Howard <hello@jaydanhoward.com>
2023-06-20 21:22:44 -0700

    lint

--- main.py
+++ main.py
@@ -1,20 +1,25 @@
 from string import Template
 
-class FlattenedPacket(dict):
 
-    # Python dict ordering arbitrary but we want to print consistently by sorting keys
+class FlattenedPacket(dict):
+    # Python dict ordering arbitrary but we want to print consistently by
+    # sorting keys
     def __repr__(self):
         res = "{"
         for i, key in enumerate(sorted(self.keys())):
-            res += Template("'$k': $v, ").substitute(k=key, v=self[key]) if (i < len(self.keys()) - 1) else Template("'$k': $v").substitute(k=key, v=self[key])
+            res += (
+                Template("'$k': $v, ").substitute(k=key, v=self[key])
+                if (i < len(self.keys()) - 1)
+                else Template("'$k': $v").substitute(k=key, v=self[key])
+            )
         res += "}"
         return res
 
+
 class PacketManager:
-    def __init__(self, delimiter = '.'):
+    def __init__(self, delimiter="."):
         self.data = []
         self.delimiter = delimiter
-
 
     def add(self, input_packet):
         validate_packet(input_packet)
@@ -22,46 +27,71 @@ class PacketManager:
         self.data.append(flattened)
 
     def __repr__(self):
-        res = ''
+        res = ""
         for packet in self.data:
             res += repr(packet)
         return res
 
 
-
 def validate_packet(input_packet):
-    if not isinstance(input_packet, (dict, float, int, str)):
-        raise TypeError(Template('Invalid packet: $p').substitute(p=input_packet))
+    if not isinstance(input_packet, dict):
+        raise TypeError(
+            Template("Invalid packet: $p").substitute(p=input_packet)
+        )
 
-def flatten_packet(input_packet, delimiter = '.'):
+    for value in input_packet:
+        if isinstance(value, dict):
+            validate_packet(value)
+        else:
+            if not isinstance(input_packet, (dict, float, int, str)):
+                raise TypeError(
+                    Template("Invalid packet: $p").substitute(p=input_packet)
+                )
+
+
+def flatten_packet(input_packet, delimiter="."):
     validate_packet(input_packet)
     flattened = FlattenedPacket()
-    stack = [{ 'obj': input_packet, 'prefix' : '' }]
+    stack = [{"obj": input_packet, "prefix": ""}]
 
     # Push nested objects onto stack until we have flattened keys
     while stack:
         current_obj, prefix = stack.pop().values()
 
-        for key, value in current_obj.items() :
-            flat_key = Template('$p$d$k').substitute(p=prefix, d=delimiter, k=key) if (prefix) else key
+        for key, value in current_obj.items():
+            flat_key = (
+                Template("$p$d$k").substitute(p=prefix, d=delimiter, k=key)
+                if (prefix)
+                else key
+            )
 
             if isinstance(value, dict):
-                stack.append({ 'obj': value, prefix: flat_key  })
+                stack.append({"obj": value, prefix: flat_key})
             else:
-                flattened[flat_key] =  value
+                flattened[flat_key] = value
     return flattened
 
-def flatten_packet_recursive(input_packet, prefix = '', delimiter = '.', validated_packet = False):
+
+def flatten_packet_recursive(
+    input_packet, prefix="", delimiter=".", validated_packet=False
+):
     if not validated_packet:
         validate_packet(input_packet)
 
     flattened = FlattenedPacket()
 
-    for key, value in input_packet.items() :
-        flat_key = Template('$p$d$k').substitute(p=prefix, d=delimiter, k=key) if (prefix) else key
+    for key, value in input_packet.items():
+        flat_key = (
+            Template("$p$d$k").substitute(p=prefix, d=delimiter, k=key)
+            if (prefix)
+            else key
+        )
         if isinstance(value, dict):
-            flattened = { **flattened, **flatten_packet_recursive(value, flat_key, delimiter, True) }
+            flattened = {
+                **flattened,
+                **flatten_packet_recursive(value, flat_key, delimiter, True),
+            }
         else:
-            flattened[flat_key] =  value
+            flattened[flat_key] = value
 
     return flattened
--- test.py
+++ test.py
@@ -3,41 +3,19 @@ from main import flatten_packet, flatten_packet_recursive
 import io
 import sys
 
-packet = {
-    'a': 1,
-    'b': 2,
-    'c': {
-        'd': 3,
-        'e': {
-            'f': 4
-        },
-        'g': 5
-    }
-}
+packet = {"a": 1, "b": 2, "c": {"d": 3, "e": {"f": 4}, "g": 5}}
 
 jumbled_small_packet = {
-    'c': {
-        'e': {
-            'f': 4
-        },
-        'g': 5,
-        'd': 3,
+    "c": {
+        "e": {"f": 4},
+        "g": 5,
+        "d": 3,
     },
-    'a': 1,
-    'b': 2,
+    "a": 1,
+    "b": 2,
 }
 
-float_packet = {
-    'a': 1.0,
-    'b': 2.0,
-    'c': {
-        'd': 3.0,
-        'e': {
-            'f': 4.0
-        },
-        'g': 5.0
-    }
-}
+float_packet = {"a": 1.0, "b": 2.0, "c": {"d": 3.0, "e": {"f": 4.0}, "g": 5.0}}
 
 
 deep_nested_packet = input_json = {
@@ -53,9 +31,7 @@ deep_nested_packet = input_json = {
                         "nested6": {
                             "nested7": {
                                 "nested8": {
-                                    "nested9": {
-                                        "nested10": "final_value"
-                                    }
+                                    "nested9": {"nested10": "final_value"}
                                 }
                             }
                         }
@@ -63,53 +39,88 @@ deep_nested_packet = input_json = {
                 }
             }
         }
-    }
+    },
 }
+
 
 class FlattenTest(unittest.TestCase):
     def test_small_packet(self):
-        self.assertEqual(flatten_packet(packet), {'a': 1, 'b': 2, 'c.d': 3, 'c.e.f': 4, 'c.g': 5})
+        self.assertEqual(
+            flatten_packet(packet),
+            {"a": 1, "b": 2, "c.d": 3, "c.e.f": 4, "c.g": 5},
+        )
 
     def test_small_packet_recursive(self):
-        self.assertEqual(flatten_packet_recursive(packet), {'a': 1, 'b': 2, 'c.d': 3, 'c.e.f': 4, 'c.g': 5})
+        self.assertEqual(
+            flatten_packet_recursive(packet),
+            {"a": 1, "b": 2, "c.d": 3, "c.e.f": 4, "c.g": 5},
+        )
+
+    def test_jumbled_small_packet(self):
+        self.assertEqual(
+            flatten_packet(packet),
+            {"a": 1, "b": 2, "c.d": 3, "c.e.f": 4, "c.g": 5},
+        )
 
     def test_jumbled_small_packet_recursive(self):
-        self.assertEqual(flatten_packet_recursive(packet), {'a': 1, 'b': 2, 'c.d': 3, 'c.e.f': 4, 'c.g': 5})
-
-    def test_jumbled_small_packet_recursive(self):
-        self.assertEqual(flatten_packet_recursive(packet), {'a': 1, 'b': 2, 'c.d': 3, 'c.e.f': 4, 'c.g': 5})
+        self.assertEqual(
+            flatten_packet_recursive(packet),
+            {"a": 1, "b": 2, "c.d": 3, "c.e.f": 4, "c.g": 5},
+        )
 
     def test_float_packet(self):
-        self.assertEqual(flatten_packet(float_packet), {'a': 1.0, 'b': 2.0, 'c.d': 3.0, 'c.e.f': 4.0, 'c.g': 5.0})
+        self.assertEqual(
+            flatten_packet(float_packet),
+            {"a": 1.0, "b": 2.0, "c.d": 3.0, "c.e.f": 4.0, "c.g": 5.0},
+        )
 
     def test_float_packet_recursive(self):
-        self.assertEqual(flatten_packet_recursive(float_packet), {'a': 1.0, 'b': 2.0, 'c.d': 3.0, 'c.e.f': 4.0, 'c.g': 5.0})
+        self.assertEqual(
+            flatten_packet_recursive(float_packet),
+            {"a": 1.0, "b": 2.0, "c.d": 3.0, "c.e.f": 4.0, "c.g": 5.0},
+        )
 
     def test_nested_packet(self):
-        self.assertEqual(flatten_packet(deep_nested_packet), {'age': 30, 'name': 'John Doe', 'nested1.nested2.nested3.nested4.nested5.nested6.nested7.nested8.nested9.nested10': 'final_value', 'top_property1': 'value1', 'top_property2': 'value2'})
+        self.assertEqual(
+            flatten_packet(deep_nested_packet),
+            {
+                "age": 30,
+                "name": "John Doe",
+                "nested1.nested2.nested3.nested4.nested5.nested6.nested7.nested8.nested9.nested10": "final_value",
+                "top_property1": "value1",
+                "top_property2": "value2",
+            },
+        )
 
 
 class PrintTest(unittest.TestCase):
     def test_small_packet(self):
-        captured_output = io.StringIO()          # Create StringIO object
-        sys.stdout = captured_output                   #  and redirect stdout.
+        captured_output = io.StringIO()  # Create StringIO object
+        sys.stdout = captured_output  # and redirect stdout.
 
         flattened = flatten_packet(packet)
-        print(flattened, end='')
+        print(flattened, end="")
 
-        sys.stdout = sys.__stdout__                   # Reset redirect.
+        sys.stdout = sys.__stdout__  # Reset redirect.
 
-        self.assertEqual(captured_output.getvalue(), "{'a': 1, 'b': 2, 'c.d': 3, 'c.e.f': 4, 'c.g': 5}")
+        self.assertEqual(
+            captured_output.getvalue(),
+            "{'a': 1, 'b': 2, 'c.d': 3, 'c.e.f': 4, 'c.g': 5}",
+        )
 
     def test_small_packet_recursive(self):
-        captured_output = io.StringIO()          # Create StringIO object
-        sys.stdout = captured_output                   #  and redirect stdout.
+        captured_output = io.StringIO()  # Create StringIO object
+        sys.stdout = captured_output  # and redirect stdout.
 
         flattened = flatten_packet_recursive(packet)
-        print(flattened, end='')
+        print(flattened, end="")
 
-        sys.stdout = sys.__stdout__                   # Reset redirect.
+        sys.stdout = sys.__stdout__  # Reset redirect.
 
-        self.assertEqual(captured_output.getvalue(), "{'a': 1, 'b': 2, 'c.d': 3, 'c.e.f': 4, 'c.g': 5}")
+        self.assertEqual(
+            captured_output.getvalue(),
+            "{'a': 1, 'b': 2, 'c.d': 3, 'c.e.f': 4, 'c.g': 5}",
+        )
+
 
 unittest.main()
